4f5b696dcc772635b639de9ed02280a7c82c48b1
danielpar04 <111545380+danielpar04@users.noreply.github.com>
2022-11-17 02:50:27 -0500

    Encapsulamiento

---
+++ Encapsulamiento/src/encapsulamiento/Clas1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encapsulamiento;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Clas1 {
+    private int edad;
+    
+    public void SetEdad(int edad){
+            this.edad=edad;
+  }                  
+    
+    public int GetEdad(){
+      return edad;  
+    }
+}
---
+++ Encapsulamiento/src/encapsulamiento/Clas2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encapsulamiento;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Clas2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Clas1 objeto1=new Clas1();
+          
+     objeto1.SetEdad(10);
+     System.out.println("La edad es: "+objeto1.GetEdad());
+    }
+    
+}
